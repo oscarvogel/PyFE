90c6488299a4be63260b48293635604291deae02
Jose Oscar Vogel <oscarvogel@gmail.com>
2020-03-19 17:12:22 -0300

    Solucionado generacion de ejecutable. Actualizado requerimientos

--- compila.bat
+++ compila.bat
@@ -1,2 +1,2 @@
 rd /S /Q dist\main
-pyinstaller --clean --win-private-assemblies --version-file=version.txt --windowed -F --icon="imagenes\Logo S-01.ico" main.py 
+pyinstaller --clean --win-private-assemblies --version-file=version.txt -w -F --icon="imagenes\Logo S-01.ico" main.py 
